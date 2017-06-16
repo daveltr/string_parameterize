5d162c819025f802ead4f04b8839569c04d31034
DT - K <k@ringzerodesign.com>
2017-06-16 09:06:18 -0400

    issue #deps

--- mix.exs
+++ mix.exs
@@ -7,8 +7,8 @@ defmodule StringParameterize.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps,
-     package: package
+     deps: deps(),
+     package: package()
    ]
   end
 
